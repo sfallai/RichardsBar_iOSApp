f928053d17706ba42c19db273758d23347a56d7a
Simon Fallai <sfallai@Simons-MacBook-Air-3.local>
2014-12-27 14:46:01 -0600

    latest

---
+++ RichardsBar_iOSApp/IDSongResult.h
@@ -0,0 +1,13 @@
+//
+//  IDSongResult.h
+//  RichardsBar_iOSApp
+//
+//  Created by Simon Fallai on 12/26/14.
+//  Copyright (c) 2014 RagingAlcoholics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDSongResult : UIViewController
+
+@end
--- RichardsBar_iOSApp/NavigationControllerSubClass.m
+++ RichardsBar_iOSApp/NavigationControllerSubClass.m
@@ -30,21 +30,12 @@
     //add a common toolbar for every view
     UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 44, self.view.frame.size.width, 44)];
     
-    //
     UIBarButtonItem *spacer = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil];
-    UIBarButtonItem *button = [[UIBarButtonItem alloc] init];
     UIImage* buttonImage = [UIImage imageNamed:@"audiowave"];
-    UIBarButtonItem *b = [[UIBarButtonItem alloc] initWithImage:buttonImage style:UIBarButtonItemStylePlain target:self action:@selector(btnIDSong_Click:)];
+    UIBarButtonItem *button = [[UIBarButtonItem alloc] initWithImage:buttonImage style:UIBarButtonItemStylePlain target:self action:@selector(btnIDSong_Click:)];
 
-    
-    //set button appearance
-    [button setImage:buttonImage];
-    
-    //set button action
-    [button setAction:@selector(btnIDSong_Click:)];
-    
     //add the buttons to the toolbar
-    [toolbar setItems:[[NSArray alloc] initWithObjects:spacer, b, spacer, nil] animated:YES];
+    [toolbar setItems:[[NSArray alloc] initWithObjects:spacer, button, spacer, nil] animated:YES];
     
     //set the toolbar appearance
     toolbar.barTintColor = [UIColor blackColor];
@@ -55,7 +46,17 @@
 }
 
 -(void) btnIDSong_Click: (id) sender {
-    [self performSegueWithIdentifier:@"idSongResultSegue" sender:self];
+    UIViewController *currentVC = self.navigationController.visibleViewController;
+    
+    UIAlertView *alert = [[UIAlertView alloc]
+                          initWithTitle:@"Your current view controller:" message:NSStringFromClass([currentVC class]) delegate:nil
+                          cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [alert show];
+    
+    //if(![vc isEqualToString:@"Song Info"]) {
+        [self performSegueWithIdentifier:@"idSongResultSegue" sender:self];
+    //}
+    
     
 }
 
